58ffe99d54b73bb11c5e93bea6ae339bf7fb8afd
helgastogova <helgastogova@gmail.com>
2021-09-15 19:39:23 +0300

    fix link

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -12,7 +12,7 @@ const Layout: React.FC<LayoutProps> = ({ children, showBackLink = false }) => {
     <div className={styles.root}>
       {showBackLink && (
         <div className={styles.link}>
-          <Link href="/">Back to main page</Link>
+          <Link href="/?type=all">Back to main page</Link>
         </div>
       )}
       <div className={styles.main}>{children}</div>
